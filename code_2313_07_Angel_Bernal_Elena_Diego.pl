2d371aee087290e39b1372c7f49f12749d3858aa
EminentNebula71 <47531350+EminentNebula71@users.noreply.github.com>
2021-04-17 19:22:35 +0200

    Add files via upload

--- code_2313_07_Angel_Bernal_Elena_Diego.pl
+++ code_2313_07_Angel_Bernal_Elena_Diego.pl
@@ -82,8 +82,15 @@ sublista(L, Menor, Mayor, E, Sublista) :- slice(L, Menor, Mayor, Sublista),
 *               Rejilla: Vector de numeros de valor real resultante con la rejilla.
 *
 ****************/
-espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla) :- print('Error. Este ejercicio no esta implementado todavia.'), !, fail.
+range(I,I,_Const,[I]).
+range(I,K,Const,[I|L]) :- I < K, I1 is I + Const, range(I1,K,Const,L).
 
+espacio_lineal(Menor, Mayor, _, _) :- Menor > Mayor, write("ERROR 4.1 Indices").
+
+espacio_lineal(_, _, Numero_elementos, _) :- Numero_elementos < 1, write("ERROR 4.1 Indices").
+
+espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla) :- Const is ((Mayor - Menor)/(Numero_elementos - 1)),
+    														range(Menor, Mayor, Const, Rejilla).
 /***************
 * EJERCICIO 5. normalizar/2
 *
